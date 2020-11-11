057df45633ced381fd404368c67df2c8dc4a032a
Santiago <santo1996.29@gmail.com>
2020-11-11 12:28:55 -0400

    feature: App component starting contents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from "react";
+import styled from "@emotion/styled";
+import cryptoImage from "./criptomonedas.png";
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: "Bebas Neue", cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+`;
 
 function App() {
   return (
-    <h1>Buscador de Criptomonedas</h1>
+    <Container>
+      <div>
+        <Image src={cryptoImage} alt="Imagen criptomoneda" />
+      </div>
+      <div>
+        <Heading>Busca precios de criptomonedas al instante</Heading>
+      </div>
+    </Container>
   );
 }
 
