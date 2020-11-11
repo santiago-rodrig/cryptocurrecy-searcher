d338cf4ac7da25a4f637b01b85cb467f02e6f57a
Santiago <santo1996.29@gmail.com>
2020-11-11 12:37:11 -0400

    fix: formatting of Form file

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import styled from '@emotion/styled'
+import React from "react";
+import styled from "@emotion/styled";
 
 const SubmitButton = styled.input`
   margin-top: 20px;
   font-weight: bold;
   font-size: 20px;
   padding: 10px;
-  background-color: #66A2FE;
+  background-color: #66a2fe;
   width: 100%;
   border-radius: 10px;
-  color: #FFF;
+  color: #fff;
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #326AC0;
+    background-color: #326ac0;
     cursor: pointer;
   }
-`
+`;
 
 const Form = () => {
   return (
     <form>
       <SubmitButton type="submit" value="Buscar" />
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
