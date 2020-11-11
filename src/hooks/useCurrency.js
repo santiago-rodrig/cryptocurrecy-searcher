c2945d8cab69a3d4c6a0a609532cc2a787946cb9
Santiago <santo1996.29@gmail.com>
2020-11-11 14:45:16 -0400

    feature: useCurrency styled components

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -1,12 +1,33 @@
 import React, { Fragment, useState } from "react";
+import styled from "@emotion/styled";
+
+const Label = styled.label`
+  font-family: "Bebas Neue", cursive;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 2.4rem;
+  margin-top: 2rem;
+  display: block;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  --webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  font-size: 0.9rem;
+`;
 
 const useCurrency = (labelText, initialState, currencies) => {
   const [state, setState] = useState(initialState);
 
   const CurrencySelect = () => (
     <>
-      <label>{labelText}</label>
-      <select value={state} onChange={(e) => setState(e.target.value)}>
+      <Label>{labelText}</Label>
+      <Select value={state} onChange={(e) => setState(e.target.value)}>
         <option value="" disabled>
           -- Elige una moneda --
         </option>
@@ -15,7 +36,7 @@ const useCurrency = (labelText, initialState, currencies) => {
             {c.name}
           </option>
         ))}
-      </select>
+      </Select>
     </>
   );
 
